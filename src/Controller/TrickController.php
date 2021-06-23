016889bca9106d4753f2d5096c7669a01879aae4
LordGeck <lordgeck@gmail.com>
2021-06-23 13:04:21 +0200

    Adding trick edition page

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -37,4 +37,22 @@ class TrickController extends AbstractController
             'form' => $form->createView()
         ]);
     }
+
+    #[Route('/trick/edit/{slug}', name: 'trick_edit')]
+    public function edit(string $slug, Request $request, TrickRepository $repository): Response
+    {
+        $trick = $repository->findOneBySlug($slug);
+        $form = $this->createForm(TrickType::class, $trick, ['trick' => $trick]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->trickManager->persist($trick);
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('trick/edit.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
 }
