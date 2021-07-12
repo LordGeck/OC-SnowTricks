3ef1d6ed98c1c17ed1de8fa391a2486cce028255
LordGeck <lordgeck@gmail.com>
2021-07-12 22:56:29 +0200

    Adding homepage content with basic tricks listing

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'home')]
-    public function home(): Response
+    public function home(TrickRepository $repository): Response
     {
-        return $this->render('home.html.twig');
+        $tricks = $repository->findAll();
+
+        return $this->render('home.html.twig', ['tricks' => $tricks]);
     }
 }
