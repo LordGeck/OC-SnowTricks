3a711d1b4b318e6b1bf3fda67128cc95a170f635
Garald <lordgeck@gmail.com>
2021-07-21 13:01:40 +0200

    Load more tricks script ajustement (#23)
    * Load more tricks script ajustement

--- assets/load-more-item.js
+++ assets/load-more-item.js
@@ -3,11 +3,12 @@ import $ from 'jquery';
 $(document).ready(function(){
     var click = 1;
     var buttonId = '#js-load-more-item';
+    var itemLimit = $(buttonId).data('itemToLoad');
     $(buttonId).on('click', function(event) {
         event.preventDefault();
-        var itemToLoad = $(buttonId).data('itemToLoad') * click;
-        click++;
-        $.get(this.href, {itemToLoad: itemToLoad}, function(data) {
+        var itemOffset = itemLimit * click;
+        click ++ ;
+        $.get(this.href, {itemLimit: itemLimit, itemOffset: itemOffset}, function(data) {
             $('#js-item-target').append(data.html);
         }, 'json');
     });
