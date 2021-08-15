7ec38511377f94afb1fec705a3eecdb204177e87
LordGeck <lordgeck@gmail.com>
2021-08-15 10:56:22 +0200

    Password reset request and password change form

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -54,6 +54,11 @@ class User implements UserInterface
      */
     private $comments;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $resetToken;
+
     public function __construct()
     {
         $this->tricks = new ArrayCollection();
@@ -204,6 +209,18 @@ class User implements UserInterface
                 $comment->setUser(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getResetToken(): ?string
+    {
+        return $this->resetToken;
+    }
+
+    public function setResetToken(?string $resetToken): self
+    {
+        $this->resetToken = $resetToken;
 
         return $this;
     }
