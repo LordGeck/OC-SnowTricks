d44d05a8f6cb9a875cccf0f20d0598be0286096a
Garald <lordgeck@gmail.com>
2021-08-17 13:47:08 +0200

    Fix for forms (#30)

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -17,7 +17,7 @@ class CommentType extends AbstractType
         ;
     }
 
-    public function configureOptions(OptionsResolver $resolver)
+    public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => Comment::class,
--- src/Form/ImageType.php
+++ src/Form/ImageType.php
@@ -15,8 +15,11 @@ class ImageType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class)
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+            ])
             ->add('file', FileType::class, [
+                'label' => false,
                 'required' => false,
             ])
         ;
