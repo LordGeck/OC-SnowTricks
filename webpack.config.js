9c74cd75b4bff52a52906161a11776609d6e6aeb
Garald <lordgeck@gmail.com>
2021-08-17 03:17:04 +0200

    Adding an hide media button (#29)

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,7 @@ Encore
     .addEntry('app', './assets/app.js')
     .addEntry('form-collection', './assets/form-collection.js')
     .addEntry('load-more-item', './assets/load-more-item.js')
+    .addEntry('hide-media', './assets/hide-media.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
